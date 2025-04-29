77f41860a31921628c013466d20a863a6e965222
Mohan <84113216+mohan08sin@users.noreply.github.com>
2025-04-29 16:02:32 +0530

    Update DepartmentService.js

--- src/Services/DepartmentService.js
+++ src/Services/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEPARTMENT_REST_API_BASE_URL = "http://localhost:8080/api/departments";
+const DEPARTMENT_REST_API_BASE_URL = "https://mohan08sin-employee-management-be-2.onrender.com";
 
 export const getAllDepartments = () => axios.get(DEPARTMENT_REST_API_BASE_URL);
 
