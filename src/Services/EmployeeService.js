4e7fee60db07a72488092cd9f3b1e34979b91da8
Mohan <84113216+mohan08sin@users.noreply.github.com>
2025-04-29 16:03:26 +0530

    Update EmployeeService.js

--- src/Services/EmployeeService.js
+++ src/Services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/employees";
+const REST_API_BASE_URL = "https://mohan08sin-employee-management-be-2.onrender.com";
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
